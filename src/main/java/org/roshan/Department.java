8f2afcf122a8f4ff2e427d7c8cb2cad39f07b024
Roshan <roshansamyla@gmail.com>
2025-12-09 23:01:12 -0500

    Added single argument constructor

--- src/main/java/org/roshan/Department.java
+++ src/main/java/org/roshan/Department.java
@@ -2,13 +2,16 @@ package org.roshan;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @EqualsAndHashCode
 @Getter
+@ToString
 public class Department {
     private String departmentId;
     private String departmentName;
-    private static int nextId = 0;
+
+    private static int nextId = 1;
 
     public static boolean isDepartmentNameValid(String departmentName) {
         if (departmentName == null || departmentName.isEmpty()) {
@@ -23,4 +26,13 @@ public class Department {
         return true;
     }
 
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+            this.departmentId = String.format("D%02d", nextId++);
+        } else  {
+            this.departmentName = null;
+            this.departmentId = null;
+        }
+    }
 }
