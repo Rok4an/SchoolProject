0e00d50ce45aac1d63664c38f0b0f7ab0813f382
Roshan <roshansamyla@gmail.com>
2025-12-14 15:32:29 -0500

    Added an all argument constructor. register course method, and Gender enum

---
+++ src/main/java/org/roshan/Student.java
@@ -0,0 +1,45 @@
+package org.roshan;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+
+    private static int nextId = 1;
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<>();
+        this.studentId = String.format("S%06d", nextId++);
+    }
+
+    public boolean registerCourse(Course course) {
+        if (registeredCourses.contains(course)) {
+            return false;
+        }
+        if (course.registerStudent(this)) {
+            registeredCourses.add(course);
+            return true;
+        }
+        return false;
+    }
+
+    public enum Gender {
+        MALE, FEMALE
+    }
+}
