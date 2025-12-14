75100e98d0540d8c95c3abe904e9adda3d4c136e
Roshan <roshansamyla@gmail.com>
2025-12-14 02:56:59 -0500

    Fixed and modified the generatedRandomScore method

--- src/main/java/org/roshan/Assignment.java
+++ src/main/java/org/roshan/Assignment.java
@@ -13,9 +13,10 @@ public class Assignment {
     private String assignmentName;
     private double weight;
     private ArrayList<Integer> scores;
+
     private static int nextId = 1;
 
-    public Assignment(String assignmentName, double weight, int maxScore) {
+    public Assignment(String assignmentName, double weight) {
         this.assignmentId = String.format("%05d",  nextId++);
         this.assignmentName = assignmentName;
         this.weight = weight;
@@ -25,44 +26,48 @@ public class Assignment {
     /**
      * Calculates the average score of this assignment.
      */
-    public void calcAssignmentAvg() {
+    public double calcAssignmentAvg() {
         double sum = 0;
-
-        for (int score : scores) {
-            sum += score;
+        for (Integer score : scores) {
+            if (score != null) {
+                sum += score;
+            }
         }
-
-        double average = sum / scores.size();
+        return scores.size() > 0 ? sum / scores.size() : 0;
     }
+
 
     /**
      * Generates random scores for all students in this assignment.
      * Scores are generated based on predefined ranges.
-     * @param numStudents the number of students to generate scores for
      */
-    public void generateRandomScore(int numStudents) {
+    public void generateRandomScore() {
         Random rand = new Random();
-        scores.clear();
 
-        for (int i = 0; i < numStudents; i++) {
-            int randomCategory = rand.nextInt(11); // [0, 10]
-            int score;
+        for (int i = 0; i < scores.size(); i++) {
+            int randomCategory = rand.nextInt(11);  // 0–10 inclusive [web:2]
+            int randomScore;
 
             if (randomCategory == 0) {
-                score = rand.nextInt(60); // [0, 60)
+                randomScore = rand.nextInt(0, 60);
+
             } else if (randomCategory == 1 || randomCategory == 2) {
-                score = 60 + rand.nextInt(10); // [60, 70)
+                randomScore = rand.nextInt(60, 70);
+
             } else if (randomCategory == 3 || randomCategory == 4) {
-                score = 70 + rand.nextInt(10); // [70, 80)
+                randomScore = rand.nextInt(70, 80);
+
             } else if (randomCategory <= 8) {
-                score = 80 + rand.nextInt(10); // [80, 90)
+                randomScore = rand.nextInt(80, 90);
+
             } else {
-                score = 90 + rand.nextInt(11); // [90, 100]
+                randomScore = rand.nextInt(90, 101);
             }
 
-            scores.add(score);
+            scores.set(i, randomScore);
         }
     }
+
 
     @Override
     public String toString() {
