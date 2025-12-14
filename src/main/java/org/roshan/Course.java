da80317dfd54a598970ebcb214c7b79a4e89de49
Roshan <roshansamyla@gmail.com>
2025-12-14 02:03:03 -0500

    Added the constructor, fields and isAssignmentWeightValid method

---
+++ src/main/java/org/roshan/Course.java
@@ -0,0 +1,44 @@
+package org.roshan;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Student> registeredStudents;
+    private ArrayList<Double> finalScores;
+
+    private static int nextId = 1;
+
+    public Course(String courseName, double credits, Department department) {
+        this.courseId = "C" + "-" + department.getDepartmentId() + "-" + String.format("%02d", nextId++);
+        this.courseName = courseName;
+        this.credits = credits;
+        this.department = department;
+        this.assignments = new ArrayList<>();
+        this.registeredStudents = new ArrayList<>();
+    }
+
+    public boolean isAssignmentWeightValid() {
+        double sum = 0;
+        double sumOfWeights = 100;
+
+        for (Assignment assignment : assignments) {
+            sum += assignment.getWeight();
+        }
+
+        return sum == sumOfWeights;
+    }
+
+}
